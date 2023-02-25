3f8b26894503751b6ff7d654dd46cb2b51b56be6
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-02-25 22:30:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def counter():
-    return 'violencedetection!!!!!'
+    return 'violencedetection!!! hello'
 
 if __name__ == '__main__':
     app.run(debug=True)
